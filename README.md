182d11ef489dcaae7bf985b33adb53279edab913
Leva <parimalleva@gmail.com>
2022-02-08 16:49:50 +0300

    delete src packed and refresh .gitignore, pom.xml, README.md

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# maven-test-one
+# maven-test-main
--- src/main/java/com/maven/example/MavenTest.java
+++
@@ -1,8 +0,0 @@
-package com.maven.example;
-
-public class MavenTest {
-  public static void main(String[] args){
-    System.out.println("Hello Maven!!!");
-  }
-
-}
