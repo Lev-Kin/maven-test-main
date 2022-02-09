667029c9e362175def0150a796a1f21752eacf14
Leva <parimalleva@gmail.com>
2022-02-09 12:43:40 +0300

    add logback refresh pom.xml change MavenTest1

--- maven-test1/src/main/java/com/maven/example/MavenTest1.java
+++ maven-test1/src/main/java/com/maven/example/MavenTest1.java
@@ -1,10 +1,16 @@
 package com.maven.example;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class MavenTest1 {
+
+  static final Logger logger = LoggerFactory.getLogger(MavenTest1.class);
 
   public static void main(String[] args) {
 
-    System.out.println("Hello Maven-test1 !!!");
+    logger.info("info - Hello Maven-test1 !!!");
+    logger.debug("debug - Hello Logback !!!");
 
   }
 
