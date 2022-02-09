f5c2393698caf022245257a4f23d75fab9a1b5fc
Leva <parimalleva@gmail.com>
2022-02-09 12:55:43 +0300

    refresh pom.xml

--- maven-test1/src/main/java/com/maven/example/MavenTest1.java
+++ maven-test1/src/main/java/com/maven/example/MavenTest1.java
@@ -2,15 +2,23 @@ package com.maven.example;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.core.util.StatusPrinter;
 
 public class MavenTest1 {
 
-  static final Logger logger = LoggerFactory.getLogger(MavenTest1.class);
+  //static final Logger logger = LoggerFactory.getLogger(MavenTest1.class);
 
   public static void main(String[] args) {
 
+    Logger logger = LoggerFactory.getLogger("com.maven.example.MavenTest1");
+
     logger.info("info - Hello Maven-test1 !!!");
     logger.debug("debug - Hello Logback !!!");
+
+    // print internal state
+    LoggerContext lc = (LoggerContext) LoggerFactory.getILoggerFactory();
+    StatusPrinter.print(lc);
 
   }
 
