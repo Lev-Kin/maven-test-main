090caf8094e5e98cd4aeb84a0177fd273d514bf6
Leva <parimalleva@gmail.com>
2022-02-10 12:07:27 +0300

    just work process

---
+++ maven-test1/src/main/java/com/maven/example/Foo.java
@@ -0,0 +1,14 @@
+package com.maven.example;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Foo {
+
+  final static Logger logger = LoggerFactory.getLogger(Foo.class);
+
+  public void doIt() {
+    logger.debug("Did it! - Foo.class");
+  }
+
+}
