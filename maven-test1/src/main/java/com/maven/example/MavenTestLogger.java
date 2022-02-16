3e71fc0e46a3ca372b0702c1aff9283bfd2a393f
Leva <parimalleva@gmail.com>
2022-02-16 19:34:55 +0300

    study process commit

--- maven-test1/src/main/java/com/maven/example/MavenTestLogger.java
+++ maven-test1/src/main/java/com/maven/example/MavenTestLogger.java
@@ -3,7 +3,6 @@ package com.maven.example;
 import ch.qos.logback.classic.LoggerContext;
 import ch.qos.logback.core.status.OnConsoleStatusListener;
 import ch.qos.logback.core.status.StatusManager;
-import com.sun.org.apache.xml.internal.serialize.LineSeparator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
